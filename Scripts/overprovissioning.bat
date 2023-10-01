ede4c179397a3b60de3749153db9db96d53b6cad
jeremiassamuelzitnik <jeremiastec185@gmail.com>
2023-09-30 21:53:33 -0300

    Update overprovissioning.bat

--- Scripts/overprovissioning.bat
+++ Scripts/overprovissioning.bat
@@ -4,6 +4,7 @@ color 2
 set /p "OverProv=Overprovisioning en (MB): "
 echo select vol W > "%temp%\shrink.txt"
 echo shrink desired=%OverProv% >> "%temp%\shrink.txt"
+echo exit >> "%temp%\shrink.txt"
 
 diskpart /s "%temp%\shrink.txt"
 diskpart /s "%~d0%~p0get-disks.txt"
